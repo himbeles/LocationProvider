614df741077a12f2085fe9f8e39768ce26c97d3c
Luis R <lri@me.com>
2020-02-29 15:37:43 +0100

    initial commit

--- README.md
+++ README.md
@@ -1,3 +1,69 @@
 # LocationProvider
 
-A description of this package.
+A Combine-based CoreLocation provider.
+
+On every update of the device location from a wrapped `CLLocationManager`,
+it provides the latest location as a published `CLLocation` object and
+via a `PassthroughSubject<CLLocation, Never>` called `locationWillChange`.
+
+## Usage
+
+### Starting the LocationProvider
+
+Initialize and start the LocationProvider
+
+```swift
+let locationProvider = LotionProvider()
+
+do {
+    try locationProvider.start()
+}
+catch LocationProviderError.noAuthorization {
+    // handle the lack of authorization, e.g. by
+    // locationProvider.requestAuthorization()
+}
+catch {
+    print("Unexpected error: \(error).")
+}
+```
+
+Potential location access authorization errors `LocationProviderError.noAuthorization` need to be caught.
+
+
+### Requesting Location Access
+
+The standard location access user dialog can be brought up via
+```swift
+locationProvider.requestAuthorization()
+```
+
+The `requestAuthorization` function accepts a additional closure that handles the  case where access has previously been denied. 
+
+```swift
+locationProvider.requestAuthorization(){presentLocationSettingsAlert()}
+```
+The default action is to present an alert that suggests to go to the app settings screen in order to change the location settings.  
+
+### Handling the Location data
+
+Subscribe to the `locationWillChange` subject and store the returned `Cancellable`
+
+```swift
+cancellableLocation = locationProvider.locationWillChange.sink { md in
+    // handleLocation(motionData: md)
+}
+```
+
+The function `handleLocation` in the `sink` closure would be executed on every `CLLocation` object sent by the `LocationProvider`.
+
+Also, the `LocationProvider` is an ObservableObject which has a published property `location` that updates the ObservableObject.
+This dynamic property can directly be accessed in SwiftUI.
+
+### Stopping the MotionProvider
+
+Stop the `LocationProvider` and cancel the subscription
+
+```swift
+locationProvider.stop()
+cancellableLocation?.cancel()
+```
--- Sources/LocationProvider/LocationProvider.swift
+++ Sources/LocationProvider/LocationProvider.swift
@@ -1,3 +1,115 @@
-struct LocationProvider {
-    var text = "Hello, World!"
+//
+//  LocationProvider.swift
+//
+//  Created by Luis R on 27.11.19.
+//  Copyright © 2019 himbeles. All rights reserved.
+//
+
+import Foundation
+import CoreLocation
+import Combine
+import UIKit
+
+/**
+ A Combine-based CoreLocation provider.
+ 
+ On every update of the device location from a wrapped `CLLocationManager`,
+ it provides the latest location as a published `CLLocation` object and
+ via a `PassthroughSubject<CLLocation, Never>` called `locationWillChange`.
+ */
+public class LocationProvider: NSObject, ObservableObject {
+    
+    private let lm = CLLocationManager()
+    
+    /// Is emitted when the `location` property changes.
+    public let locationWillChange = PassthroughSubject<CLLocation, Never>()
+    
+    /**
+     The latest location provided by the `CLLocationManager`.
+     
+     Updates of its value trigger both the `objectWillChange` and the `locationWillChange` PassthroughSubjects.
+     */
+    @Published public private(set) var location: CLLocation? {
+        willSet {
+            locationWillChange.send(newValue ?? CLLocation())
+        }
+    }
+    
+    /// The authorization status for CoreLocation.
+    @Published public var authorizationStatus: CLAuthorizationStatus?
+    
+    public override init() {
+        super.init()
+        
+        self.lm.delegate = self
+        self.lm.desiredAccuracy = kCLLocationAccuracyBest
+        self.lm.requestWhenInUseAuthorization()
+        
+        self.lm.activityType = .fitness
+        self.lm.distanceFilter = 10
+        self.lm.allowsBackgroundLocationUpdates = true
+        self.lm.pausesLocationUpdatesAutomatically = false
+        self.lm.showsBackgroundLocationIndicator = true
+    }
+    
+    /**
+     Request location access from user.
+     
+     In case, the access has already been denied, execute the `onAuthorizationDenied` closure.
+     The default behavior is to present an alert that suggests going to the settings page.
+     */
+    public func requestAuthorization(onAuthorizationDenied : ()->Void = {presentLocationSettingsAlert()}) -> Void {
+        if self.authorizationStatus == CLAuthorizationStatus.denied {
+            
+        }
+        else {
+            self.lm.requestWhenInUseAuthorization()
+        }
+    }
+    
+    /// Start the Location Provider.
+    public func start() throws -> Void {
+        guard self.authorizationStatus == .authorizedWhenInUse || self.authorizationStatus == .authorizedAlways else {
+            throw LocationProviderError.noAuthorization
+        }
+        self.lm.startUpdatingLocation()
+    }
+    
+    /// Stop the Location Provider.
+    public func stop() -> Void {
+        self.lm.stopUpdatingLocation()
+    }
+    
+}
+
+/// Present an alert that suggests to go to the app settings screen.
+public func presentLocationSettingsAlert(alertText : String? = nil) -> Void {
+    let alertController = UIAlertController (title: "Enable Location Access", message: alertText ?? "The location access for this app is set to 'never'. Enable location access in the application settings. Go to Settings now?", preferredStyle: .alert)
+    let settingsAction = UIAlertAction(title: "Settings", style: .default) { (_) -> Void in
+        guard let settingsUrl = URL(string:UIApplication.openSettingsURLString) else {
+            return
+        }
+        UIApplication.shared.open(settingsUrl)
+    }
+    alertController.addAction(settingsAction)
+    let cancelAction = UIAlertAction(title: "Cancel", style: .default, handler: nil)
+    alertController.addAction(cancelAction)
+    UIApplication.shared.windows[0].rootViewController?.present(alertController, animated: true, completion: nil)
+}
+
+
+/// Error which is thrown for lacking localization authorization.
+public enum LocationProviderError: Error {
+    case noAuthorization
+}
+
+extension LocationProvider: CLLocationManagerDelegate {
+    public func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        self.authorizationStatus = status
+    }
+    
+    public func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let location = locations.last else { return }
+        self.location = location
+    }
 }
